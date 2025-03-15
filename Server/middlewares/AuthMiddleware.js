fdbcf2dc124a32d8b6ae3aee26847f5a1829f4ea
Kavya Khurana <148501792+kavya-khurana-07@users.noreply.github.com>
2025-03-15 16:15:21 +0530

    Add files via upload

---
+++ Server/middlewares/AuthMiddleware.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (request, response, next) => {
+    // console.log(cookies);
+    const token = request.cookies.jwt;
+    // console.log({token});
+    if (!token) return response.status(401).send("You are not authenticated!")
+    jwt.verify(token, process.env.JWT_KEY, async (err,payload) => {
+       if (err) return response.status(403).send("Token is not valid!");
+       request.userId = payload.userId;
+       next();
+    });
+};
