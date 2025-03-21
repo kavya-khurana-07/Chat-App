48ca1d446efac03fa3ee77125600db26354be2a1
Kavya Khurana <148501792+kavya-khurana-07@users.noreply.github.com>
2025-03-21 18:05:58 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/AuthRoutes.js";
 import contactsRoutes from "./routes/ContactRoutes.js";
 import setupSocket from "./socket.js";
@@ -15,31 +17,30 @@ const app = express();
 const port = process.env.PORT || 3001;
 const databaseURL = process.env.DATABASE_URL;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Enable CORS for frontend origin
 app.use(
     cors({
-        origin: ["https://chat-app-frontend-two-iota.vercel.app"],
+        origin: [process.env.ORIGIN],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true
     })
 );
 
-app.use("/uploads/profiles", express.static("uploads/profiles"));
-app.use("/uploads/files", express.static("uploads/files"));
+// Temporary file storage
+app.use("/uploads/profiles", express.static(path.join(__dirname, "temp/profiles")));
+app.use("/uploads/files", express.static(path.join(__dirname, "temp/files")));
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/contacts",contactsRoutes);
+// API Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactsRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/channel",channelRoutes);
-
-app.get("/", (req,res,next) => {
-    res.send({
-        activeStatus: true,
-        error: false
-    })
-})
+app.use("/api/channel", channelRoutes);
 
 const server = app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
@@ -47,4 +48,7 @@ const server = app.listen(port, () => {
 
 setupSocket(server);
 
-mongoose.connect(databaseURL).then(()=>console.log("DB Connection Successfull.")).catch(err=>console.log(err.message));
+mongoose.connect(databaseURL)
+    .then(() => console.log("DB Connection Successful."))
+    .catch(err => console.log(err.message));
+
