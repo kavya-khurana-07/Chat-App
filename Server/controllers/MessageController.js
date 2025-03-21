b28b5878371c37f56ee458f74ae17f5f0caa7e6f
Kavya Khurana <148501792+kavya-khurana-07@users.noreply.github.com>
2025-03-21 18:15:57 +0530

    Update MessageController.js

--- Server/controllers/MessageController.js
+++ Server/controllers/MessageController.js
@@ -1,46 +1,43 @@
 import Message from "../models/MessagesModel.js";
-import {mkdirSync, renameSync} from "fs";
+import { renameSync } from "fs";
 
-export const getMessages = async (request,response,next) => {
+export const getMessages = async (request, response, next) => {
     try {
-      const user1 = request.userId;
-      const user2 = request.body.id;
+        const user1 = request.userId;
+        const user2 = request.body.id;
 
-       if (!user1 || !user2) {
-        return response.status(400).send("Both user ID's are required.");
-       }
+        if (!user1 || !user2) {
+            return response.status(400).send("Both user ID's are required.");
+        }
 
-       const messages = await Message.find({
-        $or: [
-            { sender: user1, recipient: user2 },
-            { sender: user2, recipient: user1 }
-        ]
-       }).sort({ timestamp: 1 });
+        const messages = await Message.find({
+            $or: [
+                { sender: user1, recipient: user2 },
+                { sender: user2, recipient: user1 }
+            ]
+        }).sort({ timestamp: 1 });
 
-       return response.status(200).json({messages});
-    }
-    catch (error) {
-        console.log({error});
+        return response.status(200).json({ messages });
+    } catch (error) {
+        console.log({ error });
         return response.status(500).send("Internal Server Error");
     }
 };
 
-export const uploadFile = async (request,response,next) => {
+export const uploadFile = async (request, response, next) => {
     try {
-      if (!request.file) {
-        return response.status(400).send("File is required");
-      }
-      const date = Date.now();
-      let fileDir = `uploads/files/${date}`;
-      let fileName = `${fileDir}/${request.file.originalname}`;
+        if (!request.file) {
+            return response.status(400).send("File is required");
+        }
 
-      mkdirSync(fileDir, {recursive: true});
-      renameSync(request.file.path, fileName);
+        const date = Date.now();
+        let tempFilePath = `/tmp/${date}-${request.file.originalname}`; // Store in /tmp/
 
-       return response.status(200).json({filePath: fileName});
-    }
-    catch (error) {
-        console.log({error});
+        renameSync(request.file.path, tempFilePath);
+
+        return response.status(200).json({ filePath: tempFilePath });
+    } catch (error) {
+        console.log({ error });
         return response.status(500).send("Internal Server Error");
     }
 };
